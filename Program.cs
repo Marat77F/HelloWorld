cc3a9e12bb96cf640dcd2c7bda35a2d91a913fbd
Marat77F <67151957+Marat77F@users.noreply.github.com>
2020-10-20 17:07:41 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Russkih.Lab1.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadLine(); 
+        }
+    }
+}
